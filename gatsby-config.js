0fb7aed8ef874fad03a5d0959e72fd5b7be55669
Melvin Salas <melvinsalasrivera@gmail.com>
2019-10-30 19:57:06 -0600

    Update profile pic

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
+    title: 'Melvin Salas Blog',
     author: 'Kyle Mathews',
     description: 'A starter blog demonstrating what Gatsby can do.',
     siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
